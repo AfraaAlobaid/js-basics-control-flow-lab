434b5f7fc02a448373c8252a04eb6707b72b1357
AfraaAlobaid <afraa.obaid1@gmail.com>
2021-03-14 12:55:24 +1030

    Fill the functions

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(dis) {
+  if (dis > 2500) return "No can do.";
+  else if (dis > 2000) return "I will gladly take your thirty bucks.";
+  else if (dis <= 400) return "This one is on me!";
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city) {
+  return city === "NYC" ? "Ok, sounds good." : "No go.";
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(str) {
+  switch (str) {
+    case "generous":
+      return "Thank you so much.";
+    case "not as generous":
+      return "Thank you.";
+    default:
+      return "Bye.";
+  }
 }
